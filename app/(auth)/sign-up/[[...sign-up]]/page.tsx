7b7a88653a166fe2c6466234362666f952e5fec0
Mehdiznayzen <mehdiznayzen@gmail.com>
2024-04-14 15:55:37 +0000

    second commit

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-    return <SignUp />
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-primary-50">
+            <SignUp />
+        </div>
+    )
 }
